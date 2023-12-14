89f09fe12ce79ffe1e175106b806abb935a51249
Daniel <daniel@gmail.com>
2023-12-14 13:45:25 +0300

    5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Create a trigger to reset valid_email
+-- only when the email has been changed
+
+CREATE TRIGGER reset
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    -- Check if the email is updated
+    IF NEW.email <> OLD.email THEN
+        -- Reset valid_email to its default value or any logic you want
+        SET NEW.valid_email = 0; -- 0 means not valid
+    END IF;
+END;
