ba1688be776ccb3bbe90142505bded1325f20115
Daniel <daniel@gmail.com>
2023-12-14 12:52:43 +0300

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -8,5 +8,5 @@ BEGIN
     -- Update the quantity based on the items in the new order
     UPDATE items
     SET quantity = quantity - NEW.quantity
-    WHERE name = NEW.item_name;
+    WHERE name=NEW.item_name;
 END;
