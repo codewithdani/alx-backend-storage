8d2c091124f28f12ee205568ee90e73800fd1023
Daniel <daniel@gmail.com>
2023-12-14 13:23:40 +0300

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,9 +4,7 @@
 -- Create a trigger named 'update_quantity_after_order'
 CREATE TRIGGER update_quantity
 AFTER INSERT ON orders FOR EACH ROW
-BEGIN
     -- Update the quantity based on the items in the new order
     UPDATE items
     SET quantity = quantity - NEW.quantity
     WHERE item_id = NEW.item_id;
-END;
