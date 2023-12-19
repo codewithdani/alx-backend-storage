3c704b3b9c35b16779a75ae74b130495a676f69d
Daniel <daniel@gmail.com>
2023-12-19 19:04:13 +0300

    fix pycodestyle

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,11 +6,12 @@ from pymongo import MongoClient
 
 
 def print_nginx_logs_stats(mongo_collection):
-   """  Display the total number of logs """
+    """  Display the total number of logs """
     total_logs = mongo_collection.count_documents({})
-    print(f"{total_logs} logs where {total_logs} is the number of documents in this collection")
+    print(f"{total_logs} logs where "
+          f"{total_logs} is the number of documents in this collection")
 
-   """  Display the count of logs for each HTTP method """
+    """  Display the count of logs for each HTTP method """
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
@@ -18,9 +19,6 @@ def print_nginx_logs_stats(mongo_collection):
         print(f"\t{count} logs with method={method}")
 
     """ Display the count of logs with method=GET and path=/status """
-    special_log_count = mongo_collection.count_documents({"method": "GET", "path": "/status"})
+    special_log_count = mongo_collection.count_documents(
+            {"method": "GET", "path": "/status"})
     print(f"{special_log_count} logs with method=GET and path=/status")
-
-
-
-
