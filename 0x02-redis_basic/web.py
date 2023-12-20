02dcda22d310ac381bbeadc0bdf05c6dc99da5a8
Daniel <daniel@gmail.com>
2023-12-20 18:03:29 +0300

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,22 +14,22 @@ def count_url_access(method: Callable) -> Callable:
     a URL is accessed """
     @wraps(method)
     def wrapper(url: str, *args, **kwargs):
-        # Generate keys for count and cache
+        """ Generate keys for count and cache """
         count_key = f"count:{url}"
         cache_key = f"cache:{url}"
 
-        # Increment the count for the URL
+        """ Increment the count for the URL """
         redis_client.incr(count_key)
 
-        # Check if the result is already cached
+        """ Check if the result is already cached """
         cached_result = redis_client.get(cache_key)
         if cached_result:
             return cached_result.decode('utf-8')
 
-        # Call the original method to get the result
+        """ Call the original method to get the result """
         result = method(url, *args, **kwargs)
 
-        # Cache the result with an expiration time of 10 seconds
+        """ Cache the result with an expiration time of 10 seconds """
         redis_client.setex(cache_key, 10, result)
 
         return result
